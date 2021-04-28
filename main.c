a99ce1a54899614226650e5920c751dd9fe80f67
Daniel Alves <danielalves.ccomp@gmail.com>
2021-04-27 23:16:35 -0300

    reconstrucao da mensagem

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 void embaralha(char *s, int *numReplicacoes, TipoItem *item);
 void imprimiNumReplicacoes(int *numReplicacoes, int tam);
 void imprimiChave(TipoItem *item, int tam);
+void imprimiMensagem(char* s, int tam);
 
 int main(void)
 {
@@ -14,17 +15,19 @@ int main(void)
 
     int i, *numReplicacoes, tam;
     char str[50];
-    TipoItem *item1;
+    TipoItem *item1, *item2;
 
     /*printf("---------------------------\n");
     printf("  Árvore Binária de Busca\n");
     printf("---------------------------\n");*/
 
-    printf("Entre com a msg: ");
+    printf("\n Entre com a msg: ");
     gets(str);
-    printf("\nMENSAGEM: %s\n", str);
 
     tam = strlen(str);
+
+    printf("\n MENSAGEM: \n");
+    imprimiMensagem(str, tam);
 
     item1 = malloc(tam * sizeof(TipoItem));
     numReplicacoes = malloc(tam * sizeof(int));
@@ -32,30 +35,41 @@ int main(void)
 
     /* Preenche item */
     for(i = 0; i < strlen(str); i++){
-        item1[i].chave = i;
+        item1[i].chave = i+1;
         item1[i].pacote = str[i];
         numReplicacoes[i] = 1 + (rand() % 10);
     }
 
     /* Embaralha e imprime mensagem */
     embaralha(str, numReplicacoes, item1);
-    printf("\nMENSAGEM EMBARALHADA: \n%s\n", str);
+    printf("\n\n MENSAGEM EMBARALHADA E ID: \n", str);
+    imprimiMensagem(str, tam);printf("\n");
     imprimiChave(item1, tam);
 
 
     TipoCelula **arvore1 = (TipoCelula **)malloc(sizeof(TipoCelula *));
 
-    printf("\n\nCriação da árvore vazia\n");
+    printf("\n\n Criação da árvore vazia\n");
     CriaArvoreBinariaBuscaVazia(arvore1);
-    printf("\nVerificação se a árvore criada é vazia -> %i\n", TestaArvoreBinariaBuscaVazia(arvore1));
+    printf("\n Verificação se a árvore criada é vazia -> %i\n", TestaArvoreBinariaBuscaVazia(arvore1));
 
     /*insere itens embaralhados na arvore*/
     for(i = 0; i < strlen(str); i++){
         InsereArvoreBinariaBusca(arvore1, item1[i]);
     }
 
-    printf("\nImpressão da árvore após a inclusão dos nós\n\n");
+    printf("\n Impressão da árvore após a inclusão dos nós\n\n");
     ImprimeArvoreBinariaBusca(arvore1, 0);
+
+    /* reconstrução da mensagem original */
+    for(i = 0; i < tam; i++){
+        item2 = BuscaArvoreBinariaBusca(arvore1, i+1);
+        str[i] = item2->pacote;
+        RemoveArvoreBinariaBusca(arvore1, *item2);
+    }
+    printf("\n MENSAGEM RECONSTRUIDA: \n");
+    imprimiMensagem(str, tam);
+    getchar();
 
     return 0;
 }
@@ -100,7 +114,17 @@ void imprimiChave(TipoItem *item, int tam) {
     int i;
 
     for(i = 0; i < tam; i++) {
-        printf("%d", item[i].chave);
+        printf(" %d", item[i].chave);
+    }
+
+}
+
+void imprimiMensagem(char* s, int tam) {
+
+    int i;
+
+    for(i = 0; i < tam; i++) {
+        printf(" %c", s[i]);
     }
 
 }
